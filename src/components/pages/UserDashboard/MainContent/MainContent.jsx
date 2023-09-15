c518359a831ebde41f7271fc3dca3b548cdbe498
darshan5203 <dsojitra03@gmail.com>
2023-09-15 10:46:41 +0530

    comments removed ans stats added

--- src/components/pages/UserDashboard/MainContent/MainContent.jsx
+++ src/components/pages/UserDashboard/MainContent/MainContent.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import MiniCalender from "./MiniAcademicCalendar/MiniAcademicCalendar";
 import RecentTemplates from "./RecentTemplates/RecentTemplates";
+import Statistics from"./Statistics/Statistics";
 
 const MainContent = () => {
     return (
         <div>
            <MiniCalender />
            <RecentTemplates />
+           <Statistics />
         </div>
     );
 };
